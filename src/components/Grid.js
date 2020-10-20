02be175b7844c7634662fe8f0e088b73226b4437
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-10-20 13:19:49 -0700

    Users can now add walls to the grid. Added more props to nodes.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
 import Node from './Node'
 
 export default class Grid extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      nodes: []
+      grid: [],
+      mouseIsPressed: false
     }
   }
 
   componentDidMount() {
-    const nodes = new Array(this.props.height).fill(0).map(() => new Array(this.props.length).fill(0))
-    this.setState({nodes})
+    const grid = initializeGrid(20, 20)
+    this.setState({grid})
   }
 
+  handleMouseDown(row, col) {
+    const newGrid = toggleWallNodes(this.state.grid, row, col);
+    this.setState({grid: newGrid, mouseIsPressed: true})
+  }
+
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed) return;
+    const newGrid = toggleWallNodes(this.state.grid, row, col);
+    this.setState({grid: newGrid})
+  }
+
+  handleMouseUp() {
+    this.setState({mouseIsPressed: false})
+  }
+
+
   render() {
-    const {nodes} = this.state
+    const {grid} = this.state
 
     return (
       <div className="grid">
-        {nodes.map((row, rowId) => {
+        {grid.map((row, rowId) => {
           return (
             <div key={rowId}>
-              {row.map((node, nodeId) => <Node key={nodeId} /> )}
+              {row.map((node, nodeId) => {
+                const {row, col, targetNum, isStart, isWall} = node;
+                return (
+                  <Node
+                    key={nodeId}
+                    col={col}
+                    row={row}
+                    targetNum={targetNum}
+                    isStart={isStart}
+                    isWall={isWall}
+                    onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                    onMouseEnter={(row, col) => this.handleMouseEnter(row, col)}
+                    onMouseUp={() => this.handleMouseUp()}
+                  />
+                )
+              })}
             </div>
           )
         })}
@@ -32,12 +63,57 @@ export default class Grid extends Component {
   }
 }
 
-Grid.propTypes = {
-  length: PropTypes.number,
-  height: PropTypes.number
+
+
+
+/**
+ * Create the initial grid of nodes based on the given number or rows and columns
+ * @param {Integer} numRows 
+ * @param {Integer} numCols
+ * @return {Array[Array]}
+ */
+const initializeGrid = (numRows, numCols) => {
+  const grid = []
+  for (let row = 0; row < numRows; row++) {
+    const currentRow = [];
+    for (let col = 0; col < numCols; col++) {
+      currentRow.push(createNode(col, row));
+    }
+    grid.push(currentRow);
+  }
+  return grid
 }
 
-Grid.defaultProps = {
-  length: 30,
-  height: 15
+/**
+ * Generate initial node prop values based on the row and column of the node
+ * @param {Integer} col 
+ * @param {Integer} row
+ * @return {Object}
+ */
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isStart: false,
+    targetNum: null,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null
+  }
+}
+
+/**
+ * Toggle whether or not a node is a wall
+ * @param {Array[Array]} grid 
+ * @param {Integer} row 
+ * @param {Integer} col
+ * @return {Array[Array]}
+ */
+const toggleWallNodes = (grid, row, col) => {
+  const newGrid = grid.slice()
+  const node = newGrid[row][col]
+  const newNode = { ...node, isWall: !node.isWall }
+  newGrid[row][col] = newNode
+  return newGrid
 }
--- src/components/Node.js
+++ src/components/Node.js
@@ -1,8 +1,42 @@
 
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Node(props) {
+  const {
+    col,
+    row,
+    targetNum,
+    isStart,
+    isWall,
+    onMouseDown,
+    onMouseEnter,
+    onMouseUp
+  } = props
+
+  const extraClassName = targetNum !== null
+  ? 'target-node'
+  : isStart
+  ? 'start-node'
+  : isWall
+  ? 'wall-node'
+  : '';
+
   return (
-    <div className="node"/>
+    <div
+      id={`node-${row}-${col}`}
+      className={`node ${extraClassName}`}
+      onMouseDown={() => onMouseDown(row, col) }
+      onMouseEnter={() => onMouseEnter(row, col) }
+      onMouseUp={() => onMouseUp() }
+    />
   )
+}
+
+Node.propTypes = {
+  col: PropTypes.number.isRequired,
+  row: PropTypes.number.isRequired,
+  targetNum: PropTypes.number,
+  isStart: PropTypes.bool.isRequired,
+  isWall: PropTypes.bool.isRequired,
 }
